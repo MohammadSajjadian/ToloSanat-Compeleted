f9b5ddd66b69033abc17f3223e924aa845a38dbc
Mohammad Sajjadian <86977415+MohammadSajjadian@users.noreply.github.com>
2021-11-08 23:34:45 +0330

    Create CRUD controller for index elements and make view for their edit page.

---
+++ Brella/Controllers/CRUD.cs
@@ -0,0 +1,44 @@
+﻿using cloudscribe.Pagination.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brella.Controllers
+{
+    public class CRUD : Controller
+    {
+        #region KeyWords
+
+        private const string success = "success";
+        private const string error = "error";
+        private const string warning = "warning";
+        private const string info = "info";
+        private const string Index = "Index";
+        private const string Home = "Home";
+        private const string Account = "Account";
+
+        #endregion
+
+
+        #region Dependency Injections
+
+        public CRUD()
+        {
+
+        }
+
+        #endregion
+
+
+        #region CV
+
+        public IActionResult AddProject()
+        {
+
+        }
+
+        #endregion
+    }
+}
